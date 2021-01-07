9e374e01353b5513f6494ed128b90d8be0e23ff8
siykt <cnsiykt@163.com>
2021-01-07 15:41:56 +0800

    Feat: 添加自动化提交打标功能

--- api/taobao/MarkInfoAutoSubmit/chinasaltenergy/run.py
+++ api/taobao/MarkInfoAutoSubmit/chinasaltenergy/run.py
@@ -1,12 +1,15 @@
 import json
 from pathlib import Path
 from time import sleep
-
+import pandas
+from random import randint
 from utils.auto_submit import AutoModules
 
 
 class Default(AutoModules):
     domain = 'https://h5.gas.chinasaltenergy.com'
+    config_path = Path('../assets/config.json')
+    last_count = json.loads(config_path.read_text())['last_count']
 
     def run(self, user_phone_no: str):
         # XhST6K
@@ -15,35 +18,45 @@ class Default(AutoModules):
             "userPhoneNo": user_phone_no, "invitationCode": "5lU55n"
         })
 
+    def send(self, phone):
+        exit()
+        if not config_path.exists():
+            config_path.write_text('{"last_count": 0}')
+
+        self.logger.info(f'正在提交第 {self.last_count + 1} 个 电话为: {phone}')
+        self.last_count += 1
+        self.logger.info(f'提交结果 -> {self.run(phone).text}')
+        self.config_path.write_text(json.dumps({'last_count': self.last_count}))
+        # sleep_time = randint(1, 1)
+        # module.logger.info(f'暂停 {sleep_time} 秒继续')
+        # sleep(sleep_time)
+
 
 if __name__ == '__main__':
-    # import pandas
-    from random import randint
-
     config_path = Path('../assets/config.json')
-    today_upload_count = int(input('请输入个数： '))
-
-    if not config_path.exists():
-        config_path.write_text('{"last_count": 0}')
+    # today_upload_count = int(input('请输入个数： '))
+    today_upload_count = 1
 
     if str(today_upload_count)[-1] == '0':
         today_upload_count += randint(1, 9)
 
-    with Path('../assets/phone.txt').open('r') as file:
-        phones = file.read().split('\n')
+    # with Path('../assets/phone.csv').open('r') as file:
+    #     phones = file.read().split('\n')
+    phones = pandas.read_csv('../assets/phone.csv').dropna(axis=1).drop_duplicates()
+    is_csv = True
 
     module = Default()
     module.logger.setLevel('INFO')
-    last_count = json.loads(config_path.read_text())['last_count']
     try:
-        for i, phone in enumerate(phones[last_count:last_count + today_upload_count]):
-            module.logger.info(f'正在提交第 {last_count + 1} 个')
-            last_count += 1
-            module.logger.info(f'提交结果 -> {module.run(phone).text}')
-            config_path.write_text(json.dumps({'last_count': last_count}))
-            # sleep_time = randint(1, 1)
-            # module.logger.info(f'暂停 {sleep_time} 秒继续')
-            # sleep(sleep_time)
+        splice_count = ''
+        if is_csv:
+            for [phone] in phones.values[module.last_count:module.last_count + today_upload_count]:
+                module.send(phone)
+
+        else:
+            for i, phone in enumerate(phones[module.last_count:module.last_count + today_upload_count]):
+                module.send(phone)
+
     except Exception as err:
         module.logger.exception(err)
-        module.logger.info(f'在提交第 {last_count} 个时失败')
+        module.logger.info(f'在提交第 {module.last_count} 个时失败')
