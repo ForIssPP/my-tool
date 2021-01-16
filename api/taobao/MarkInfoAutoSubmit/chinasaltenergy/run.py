c496d581f2a427765f0b77a3bdeda31453f5eb80
siykt <cnsiykt@163.com>
2021-01-16 09:42:30 +0800

    Pref: 优化下载文件效率与错误捕获机制

--- api/taobao/MarkInfoAutoSubmit/chinasaltenergy/run.py
+++ api/taobao/MarkInfoAutoSubmit/chinasaltenergy/run.py
@@ -4,17 +4,19 @@ from time import sleep
 import pandas
 from random import randint
 from utils.auto_submit import AutoModules
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
 
 class Default(AutoModules):
     domain = 'https://h5.gas.chinasaltenergy.com'
     config_path = Path('../assets/config.json')
-    last_count = json.loads(config_path.read_text())['last_count']
+    last_count = 55500
 
     def run(self, user_phone_no: str):
         # invitation_code = 'XhST6K'
         # invitation_code = '5lU55n'
-        invitation_code = 'jvu3ei'
+        # invitation_code = 'jvu3ei'
+        invitation_code = 'z497sw'
         self.update_content_type('application/json;charset=UTF-8')
         return self.fetch('/mp/api/channelagent/channelinviteuser/newUserNC', json={
             "userPhoneNo": user_phone_no, "invitationCode": invitation_code
@@ -22,12 +24,13 @@ class Default(AutoModules):
 
     def send(self, phone):
         if not config_path.exists():
-            config_path.write_text('{"last_count": 0}')
+            # config_path.write_text('{"last_count": 55500}')
 
+            pass
         self.logger.info(f'正在提交第 {self.last_count + 1} 个 电话为: {phone}')
         self.last_count += 1
         self.logger.info(f'提交结果 -> {self.run(phone).text}')
-        self.config_path.write_text(json.dumps({'last_count': self.last_count}))
+        # self.config_path.write_text(json.dumps({'last_count': self.last_count}))
         # sleep_time = randint(1, 1)
         # module.logger.info(f'暂停 {sleep_time} 秒继续')
         # sleep(sleep_time)
@@ -48,11 +51,18 @@ if __name__ == '__main__':
 
     module = Default()
     module.logger.setLevel('INFO')
+    executor = ThreadPoolExecutor(max_workers=30)
+
     try:
         splice_count = ''
         if is_csv:
+            tasks = []
+
             for [phone] in phones.values[module.last_count:module.last_count + today_upload_count]:
-                module.send(phone)
+                tasks.append(executor.submit(module.send, phone))
+
+            wait(tasks, return_when=ALL_COMPLETED)
+            module.logger.info('提交完毕')
 
         else:
             for i, phone in enumerate(phones[module.last_count:module.last_count + today_upload_count]):
